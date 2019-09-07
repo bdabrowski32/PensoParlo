dc5b3fb3d6681a0cfd2b7ccf12e2d4bdf924ca35
Brendyn Dabrowski <brendyn.dabrowski@cerner.com>
2019-09-07 12:52:19 -0500

    Added realm. Added gitlfs. Added speech view controller.

---
+++ Penso Parlo/ChecklistItem.swift
@@ -0,0 +1,11 @@
+//
+//  ChecklistItem.swift
+//  Penso Parlo
+//
+//  Created by Dabrowski,Brendyn on 5/26/19.
+//  Copyright © 2019 BDCreative. All rights reserved.
+//
+
+class ChecklistItem {
+    var text = ""
+}
---
+++ Pods/Target Support Files/Pods-Penso Parlo/Pods-Penso Parlo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Penso_Parlo : NSObject
+@end
+@implementation PodsDummy_Pods_Penso_Parlo
+@end
