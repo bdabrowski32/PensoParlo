3ad67433ff6d9dd7faf1d96c34d8653eee661f09
Brendyn Dabrowski <brendyn.dabrowski@cerner.com>
2019-09-17 21:18:12 -0500

    [PENSO-3] Added doc.

--- Penso Parlo/Speech/SpeechDetectionViewController.swift
+++ Penso Parlo/Speech/SpeechDetectionViewController.swift
@@ -9,39 +9,69 @@
 import Speech
 import UIKit
 
+/**
+ This class displays the speech detection view within the storyboard. Handles all speech recognition duties.
+ */
 class SpeechDetectionViewController: UIViewController, SFSpeechRecognizerDelegate {
 
+    /// Displays the text that the user says.
     @IBOutlet private weak var detectedTextLabel: UILabel!
+
+    /// Starts the workflow to begin detecting speech.
     @IBOutlet private weak var startButton: UIButton!
 
-    let audioEngine = AVAudioEngine()
-    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
-    let request = SFSpeechAudioBufferRecognitionRequest()
+    /// Used to generate and process audio signals and perform audio input and output.
+    private let audioEngine = AVAudioEngine()
 
-    var recognitionTask: SFSpeechRecognitionTask?
+    /// Used to check for the availability of the speech recognition service, and to initiate the speech recognition process.
+    private let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+
+    /// Request to recognize speech from the device's microphone.
+    private let request = SFSpeechAudioBufferRecognitionRequest()
+
+    /// Task object used to monitor the speech recognition progress.
+    private var recognitionTask: SFSpeechRecognitionTask?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.requestSpeechAuthorization()
     }
 
-    @IBAction func startButtonTapped(_ sender: UIButton) {
-        self.recordAndRecognizeSpeech()
+    deinit {
+        self.recognitionTask = nil
     }
 
-    func recordAndRecognizeSpeech() {
+    /**
+     Action to take place when the user presses the start button on the view controller
+
+     - parameter sender: The button on the view.
+     */
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        self.dictateSpeech()
+    }
+
+    /**
+     Prepares the audio engine for speech recognition.
+     */
+    private func startAudioEngine() {
         let node = audioEngine.inputNode
-        let recordingFormat = node.outputFormat(forBus: 0)
-        node.installTap(onBus: 0, bufferSize: 1_024, format: recordingFormat) { buffer, _ in
+        node.installTap(onBus: 0, bufferSize: 1_024, format: node.outputFormat(forBus: 0)) { buffer, _ in
             self.request.append(buffer)
         }
 
-        audioEngine.prepare()
+        self.audioEngine.prepare()
         do {
-            try audioEngine.start()
+            try self.audioEngine.start()
         } catch {
             return print(error)
         }
+    }
+
+    /**
+     Performs speech dictation and updates the labels on the view.
+     */
+    private func dictateSpeech() {
+        self.startAudioEngine()
 
         guard let speechRecognizer = SFSpeechRecognizer() else {
             print("A recognizer is not supported for the current locale.")
@@ -62,28 +92,46 @@ class SpeechDetectionViewController: UIViewController, SFSpeechRecognizerDelegat
                 var lastString = ""
                 for segment in result.bestTranscription.segments {
                     let indexTo = bestString.index(bestString.startIndex, offsetBy: segment.substringRange.location)
-                    lastString = String(bestString[..<indexTo]) //.substring(from: indexTo)
+                    lastString = String(bestString[indexTo...])
                 }
 
-                self.checkForStop(resultString: lastString)
+                guard self.checkForStop(resultString: lastString) == false else {
+                    self.detectedTextLabel.text = bestString.components(separatedBy: " ").dropLast().joined(separator: " ")
+                    NotesItem.add(text: self.detectedTextLabel.text ?? "nil")
+                    print("Already stopped recording.")
+                    return
+                }
             } else if let error = error {
                 print(error)
             }
         })
     }
 
-    func checkForStop(resultString: String) {
-        if resultString == "penso" { // Dont want to say stop because that may be a cimmon word
-            self.audioEngine.stop()
-            self.startButton.isEnabled = true
+    /**
+     Turns off the audio engine.
+     */
+    private func checkForStop(resultString: String) -> Bool {
+        if resultString == "stop" { // Dont want to say stop because that may be a common word
+            self.performStop()
+            return true
         }
+
+        return false
     }
 
-    private func checkLastString(in bestTranscription: SFTranscription) {
-
+    /**
+     Turns off the audio engine and other tasks when speech dictation is ended.
+     */
+    private func performStop() {
+        self.audioEngine.stop()
+        self.performSegue(withIdentifier: "DoneSpeaking", sender: self)
+        self.recognitionTask?.cancel()
     }
 
-    func requestSpeechAuthorization() {
+    /**
+     Requests users authorization for speech dictation and updates button and labels on view accordingly.
+     */
+    private func requestSpeechAuthorization() {
         SFSpeechRecognizer.requestAuthorization { authStatus in
             OperationQueue.main.addOperation {
                 switch authStatus {
